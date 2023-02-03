ce66b5160c2e44b6fb1ea8345493988666cd9645
Mila-riez <t.rieznichok@spribe.co>
2023-02-03 21:48:43 +0200

    fix: rewrite list file data to map file data

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,45 @@
-import homework6.Product;
-import homework6.ProductManager;
-import homework6.exceptions.NoSuchElement;
-
-import java.util.*;
+import homework5.FileData;
+import homework5.FileNavigator;
 
 public class Main {
-    static ProductManager productManager = new ProductManager();
 
     public static void main(String[] args) {
-        System.out.println("books worth more than 250:");
-        getBooksCertainPrice();
-        System.out.println("discount books:");
-        getBooksWithDiscount();
-        System.out.println("cheapest book:");
-        getCheapestBook();
-        System.out.println("3 newest products:");
-        getNewestProducts();
-        System.out.println("calculate products price by filter type, price, date adding:");
-        calculateProductsPriceByParameters();
-        System.out.println("group products by type:");
-        groupProductsByType();
-    }
-
-    private static void printProducts(List<Product> products) {
-        products.forEach(product -> {
-            System.out.println(product.getType() + " " + product.getPrice() + " " + product.getDateAdding());
-        });
-    }
-
-    private static void getBooksCertainPrice() {
-        printProducts(productManager.getBooksCertainPrice());
-    }
-
-    private static void getBooksWithDiscount() {
-        printProducts(productManager.getBooksWithDiscount());
-    }
-
-    private static void getCheapestBook() {
-        try {
-            System.out.println(productManager.getCheapestBook().getType() + " " + productManager.getCheapestBook().getPrice() + " " + productManager.getCheapestBook().getDateAdding());
-        } catch (NoSuchElement e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static void getNewestProducts() {
-        printProducts(productManager.getNewestProducts());
-    }
-
-    private static void calculateProductsPriceByParameters() {
-        System.out.println(productManager.calculatePrice());
-    }
-
-    private static void groupProductsByType() {
-        productManager.groupByType().forEach((type, values) -> {
-            System.out.println(type + ":");
-            values.forEach(value -> {
-                System.out.println(value.getType() + " " + value.getPrice() + " " + value.getDateAdding());
+        FileNavigator fileNavigator = new FileNavigator();
+        fileNavigator.add(new FileData("file", (byte) 2, "path/to/file"));
+        fileNavigator.add(new FileData("file", (byte) 2, "path/to33/file"));
+        fileNavigator.add(new FileData("file", (byte) 44, "path/to/file"));
+        fileNavigator.add(new FileData("file", (byte) 32, "path2/to/file"));
+        fileNavigator.add(new FileData("file", (byte) 12, "path3/to/file"));
+        fileNavigator.add(new FileData("file66", (byte) 4, "path3/to/file"));
+        fileNavigator.add(new FileData("file", (byte) 22, "path/to/file"));
+        fileNavigator.add(new FileData("file", (byte) 2, "path/to/file"), "path/to/file2");
+        fileNavigator.add(new FileData("file5", (byte) 3, "path/to/file2"), "path/to/file2");
+        fileNavigator.getFiles().forEach((key, value) -> {
+            System.out.println("key : " + key);
+            value.forEach(el -> {
+                System.out.println("value : " + el.name() + " " + el.path() + " " + el.size());
             });
+        });
+        System.out.println("filterBySize");
+        fileNavigator.filterBySize(33).forEach((key) -> {
+            System.out.println("key : " + key.name() + " " + key.size() + " ");
+        });
+        System.out.println("find :");
+        fileNavigator.find("path/to/file").forEach((key) -> {
+            System.out.println("key : " + key.name() + " " + key.size() + " ");
+        });
+        fileNavigator.remove("path/to/file");
+
+        System.out.println("after delete: ");
+        fileNavigator.getFiles().forEach((key, value) -> {
+            System.out.println("key : " + key);
+            value.forEach(el -> {
+                System.out.println("value : " + el.name() + " " + el.path() + " " + el.size());
+            });
+        });
+        System.out.println("SORT");
+        fileNavigator.sortFiles().forEach((key) -> {
+            System.out.println("key : " + key.name() + " " + key.size() + " ");
         });
     }
 }
